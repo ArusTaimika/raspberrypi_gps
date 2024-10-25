08e070b70fa71b002a781e7823fffcaebc8cef5c
ArusTaimika <arus2.maru2@gmail.com>
2024-10-25 07:26:57 +0000

    udp tempの作成と整理

---
+++ cpp_udp_temp/udp_connect.hpp
@@ -0,0 +1,39 @@
+#ifndef UDP_CONNECT_HPP
+#define UDP_CONNECT_HPP
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <cstdlib>  // EXIT_FAILURE, exitのため
+#include <cstdio>   // perrorのため
+#include <iostream>
+
+namespace udp_lib{
+
+class UdpConnect{
+    /*
+    UDP送受信を行うクラス
+    */
+    int sock;
+    struct sockaddr_in addr;
+    
+    public:
+        UdpConnect(std::string address, int port); // UDPコンストラクタ       
+        
+        void udp_send(double value); // UDP送信関数
+
+        void udp_bind(); 
+
+        double udp_recv();
+
+        void udp_recv(char *buf, int size); // UDP受信関数
+        
+        ~UdpConnect();
+};
+
+}
+
+#endif // UDP_CONNECT_HPP  // 3. ここでガードを閉じます
