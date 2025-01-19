126856ac1e1cb5d7ba928b155dd0bdaa96dd2f5f
ArusTaimika <arus2.maru2@gmail.com>
2025-01-20 00:39:33 +0900

    カウンタとベクトルを送信

--- udp_send_time.cpp
+++ udp_send_time.cpp
@@ -23,14 +23,15 @@ int server_1(){
          パルス波形を出力初期化
         */
         GpioPulseGenerator pulseGenerator("gpiochip0", 16, 250, 500);
-
+        int roop_count = 0;
         // データ送信を無限ループで行う
         while (true) {
             //現在時刻取得
-            std::chrono::high_resolution_clock::time_point server_clock = std::chrono::high_resolution_clock::now();
-            std::chrono::nanoseconds nano_system_clock = std::chrono::duration_cast<std::chrono::nanoseconds>(server_clock.time_since_epoch());
+            // std::chrono::high_resolution_clock::time_point server_clock = std::chrono::high_resolution_clock::now();
+            // std::chrono::nanoseconds nano_system_clock = std::chrono::duration_cast<std::chrono::nanoseconds>(server_clock.time_since_epoch());
+            roop_count ++;
             // UDP送信
-            udpConnection.udp_send(dataToSend, nano_system_clock); // std::chrono::nanoseconds　はint64_tのサイズ数
+            udpConnection.udp_send(dataToSend, roop_count); // std::chrono::nanoseconds　はint64_tのサイズ数
             
             //パルス波形を出力
             pulseGenerator.start();
@@ -72,8 +73,8 @@ int receive_1(){
         csv_lib::Csvedit csvWriter("compare_time.csv");
         // csv_ヘッダを設定
         csvWriter.csv_write_headers({"server_time", "client_time", "delay_time"});
-        // 送信データの型定義
-        std::vector<std::chrono::nanoseconds>  send_data;        
+        // csvデータの型定義
+        //std::vector<std::chrono::nanoseconds>  send_data;        
 
         /*
          パルス波形を出力初期化
@@ -82,8 +83,8 @@ int receive_1(){
         // データ受信を無限ループで行う
         while (true) {
             // UDP受信
-            std::pair<std::vector<double>, std::chrono::nanoseconds> receivedData = udpConnection.udp_recv();// pairはfirst, secondで抽出可能
-            nano_server_clock = receivedData.second;
+            std::pair<std::vector<double>, int> receivedData = udpConnection.udp_recv();// pairはfirst, secondで抽出可能
+            //nano_server_clock = receivedData.second;
             // 現在時刻取得
             std::chrono::high_resolution_clock::time_point receive_clock = std::chrono::high_resolution_clock::now();
             nano_receive_clock = std::chrono::duration_cast<std::chrono::nanoseconds>(receive_clock.time_since_epoch());
@@ -92,14 +93,14 @@ int receive_1(){
             pulseGenerator.start();
             
             // delay計算
-            delay_time = nano_receive_clock - nano_server_clock;            
+            //delay_time = nano_receive_clock - nano_server_clock;            
 
             // 出力
-            std::cout << "delay time: " <<  delay_time.count() << std::endl;
+            //std::cout << "delay time: " <<  delay_time.count() << std::endl;
             //csv出力
-            send_data = {nano_server_clock, nano_receive_clock, delay_time};
+            //send_data = {nano_server_clock, nano_receive_clock, delay_time};
             // データをペア型にして書き込み
-            csvWriter.csv_write_data(send_data);
+            csvWriter.csv_write_data(receivedData);
 
             // 少し待機して次の受信へ（必要であれば待機時間を調整可能）
             std::this_thread::sleep_for(std::chrono::milliseconds(10));
