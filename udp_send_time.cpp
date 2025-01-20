2ffd89e6fc6bf78e741c628a9f8e9655303c5bb2
ArusTaimika <arus2.maru2@gmail.com>
2025-01-21 04:45:45 +0900

    2つのlocationPCからデータを受信

--- udp_send_time.cpp
+++ udp_send_time.cpp
@@ -22,7 +22,7 @@ int server_1(){
         /*
          パルス波形を出力初期化
         */
-        GpioPulseGenerator pulseGenerator("gpiochip0", 16, 250, 500);
+        //GpioPulseGenerator pulseGenerator("gpiochip0", 16, 250, 500);
         int roop_count = 0;
         // データ送信を無限ループで行う
         while (true) {
@@ -34,7 +34,7 @@ int server_1(){
             udpConnection.udp_send(dataToSend, roop_count); // std::chrono::nanoseconds　はint64_tのサイズ数
             
             //パルス波形を出力
-            pulseGenerator.start();
+            //pulseGenerator.start();
             // 出力
             std::cout << "Data sent: " ;
             std::copy(dataToSend.begin(), dataToSend.end(), std::ostream_iterator<double>(std::cout, " "));
@@ -52,13 +52,13 @@ int server_1(){
     return EXIT_SUCCESS;
 }
 
-int receive_1(){
+int receive_1(std::string from_name, int port){
     try {
         /*
          UDP通信初期化
         */
         // IPアドレスとポート番号を指定して、UdpConnectインスタンスを作成
-        udp_lib::UdpConnect udpConnection("0.0.0.0", 4000, 6);  // "0.0.0.0"はすべてのIPアドレスからの接続を受け入れる
+        udp_lib::UdpConnect udpConnection("0.0.0.0", port, 6);  // "0.0.0.0"はすべてのIPアドレスからの接続を受け入れる
         // バインド（サーバーとして動作するために必要）
         udpConnection.udp_bind();
         // システムクロック定義
@@ -70,7 +70,11 @@ int receive_1(){
          CSVクラス初期化
         */
         // 出力するcsvファイルを指定して，インスタンスを作成
-        csv_lib::Csvedit csvWriter("compare_time.csv");
+        std::string baseName = "compare_time";
+        std::string extension = ".csv";
+        // ファイル名を生成
+        std::string fileName = baseName + from_name + extension;
+        csv_lib::Csvedit csvWriter(fileName);
         // csv_ヘッダを設定
         csvWriter.csv_write_headers({"MRpx", "MRpy","MRth","CRpx", "CRpy","CRth","count","Time"});
         // csvデータの型定義  
@@ -78,7 +82,7 @@ int receive_1(){
         /*
          パルス波形を出力初期化
         */
-        GpioPulseGenerator pulseGenerator("gpiochip0", 16, 250, 500);
+        //GpioPulseGenerator pulseGenerator("gpiochip0", 16, 250, 500);
         // データ受信を無限ループで行う
         while (true) {
             // UDP受信
@@ -89,7 +93,7 @@ int receive_1(){
             nano_receive_clock = std::chrono::duration_cast<std::chrono::nanoseconds>(receive_clock.time_since_epoch());
             
             //パルス波形を出力
-            pulseGenerator.start();
+            //pulseGenerator.start();
             
             // delay計算
             //delay_time = nano_receive_clock - nano_server_clock;            
@@ -115,9 +119,12 @@ int receive_1(){
 
 int main(){
     //std::thread th1(server_1);
-    std::thread th2(receive_1);
+    std::thread th2(receive_1,"from_a", 4000);
+    std::thread th3(receive_1,"from_b", 5000);
+
 
     //th1.join();
     th2.join();
+    th3.join();
     return 0;
 }
